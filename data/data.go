75b3792217db3bfc1162b1c3b99b903b0e4e779c
Javier Galarza <jegj57@gmail.com>
2025-03-08 06:43:50 -0500

    refactor: clean up data

--- data/data.go
+++ data/data.go
@@ -7,12 +7,16 @@ import (
 
 const DEFAULT_DATA_FILENAME = "data.db"
 
+var (
+	DEFAULT_DATA_FOLDER   = filepath.Join(os.Getenv("HOME"), ".local", "share", "g3")
+	DEFAULT_DATA_FILEPATH = filepath.Join(DEFAULT_DATA_FOLDER, DEFAULT_DATA_FILENAME)
+)
+
 func CreateDataFileIfRequired() error {
-	dataDir := filepath.Join(os.Getenv("HOME"), ".local", "share", "g3")
-	if err := os.MkdirAll(dataDir, 0o700); err != nil {
+	if err := os.MkdirAll(DEFAULT_DATA_FOLDER, 0o700); err != nil {
 		return err
 	}
-	dataFilePath := filepath.Join(dataDir, DEFAULT_DATA_FILENAME)
+	dataFilePath := filepath.Join(DEFAULT_DATA_FOLDER, DEFAULT_DATA_FILENAME)
 	dataFile, err := os.OpenFile(dataFilePath, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
@@ -20,3 +24,21 @@ func CreateDataFileIfRequired() error {
 	defer dataFile.Close()
 	return nil
 }
+
+/*
+func AppendEntry(filename string, gistId string, gistPath string) error {
+	file, err := os.OpenFile(DEFAULT_DATA_FILEPATH, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	data, err := json.Marshal(entry)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(append(data, '\n'))
+	return err
+}
+*/
