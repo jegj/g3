5bf4bb29cc2553d764accc13962dc3d78b589031
Javier Galarza <jegj57@gmail.com>
2025-03-11 07:37:32 -0500

    feat: add delete and read func for data package

--- data/data.go
+++ data/data.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"os"
@@ -12,7 +13,7 @@ const DEFAULT_DATA_FILENAME = "data.jsonl"
 var (
 	DEFAULT_DATA_FOLDER   = filepath.Join(os.Getenv("HOME"), ".local", "share", "g3")
 	DEFAULT_DATA_FILEPATH = filepath.Join(DEFAULT_DATA_FOLDER, DEFAULT_DATA_FILENAME)
-	ERR_ENTRY_NOT_FOUND   = errors.New("entry not found")
+	ErrEntryNotFound      = errors.New("entry not found")
 )
 
 func CreateDataFileIfRequired() error {
@@ -47,4 +48,44 @@ func AppendEntry(filename string, gists []GistEntry) error {
 
 	_, err = file.Write(append(data, '\n'))
 	return err
+}
+
+func ReadEntries() ([]DataEntry, error) {
+	file, err := os.Open(DEFAULT_DATA_FILEPATH)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var entries []DataEntry
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var entry DataEntry
+		if err := json.Unmarshal([]byte(scanner.Text()), &entry); err != nil {
+			return nil, err
+		}
+		entries = append(entries, entry)
+	}
+	return entries, scanner.Err()
+}
+
+func DeleteEntryByFilename(filename string) error {
+	entries, err := ReadEntries()
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(DEFAULT_DATA_FILEPATH)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+
+	for _, entry := range entries {
+		if _, exist := entry[filename]; !exist {
+			data, _ := json.Marshal(entry)
+			_, _ = writer.WriteString(string(data) + "\n")
+		}
+	}
+	return nil
 }
