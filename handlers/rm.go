7d122d867d319c38320ca00e7131e0dd7414a1c6
Javier Galarza <jegj57@gmail.com>
2025-03-20 06:52:44 -0500

    feat: add rm command

--- handlers/rm.go
+++ handlers/rm.go
@@ -1,5 +1,41 @@
 package handlers
 
+import (
+	"encoding/json"
+	"log/slog"
+
+	"github.com/jegj/g3/config"
+	"github.com/jegj/g3/fsdata"
+)
+
+// TODO: Handler err when one call to github api fails and the other not
+// TODO: Parallel delete calls when the file contains more than one gist
 func (g *G3BaseHandler) Rm(filename string) error {
+	g3filepath := fsdata.GetG3Filepath(filename)
+	content, err := g.D.GetFileContent(g3filepath)
+	if err != nil {
+		return err
+	}
+
+	var entry fsdata.DataEntry
+	err = json.Unmarshal(content, &entry)
+	if err != nil {
+		return err
+	}
+
+	for filename, fileGist := range entry {
+		slog.Debug("Deleting file....", "filename", filename)
+		for _, gist := range fileGist.Gist {
+			err := g.G.DeleteGist(gist.ID, config.Conf.GHToken)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	err = g.D.DeleteEntry(g3filepath)
+	if err != nil {
+		return err
+	}
 	return nil
 }
