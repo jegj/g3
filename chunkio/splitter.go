d7b3864ed74919b47ca91e8448d1121f6b62fb86
Javier Galarza <jegj57@gmail.com>
2025-07-31 07:35:50 -0500

    refactor: forgotten file

--- chunkio/splitter.go
+++ chunkio/splitter.go
@@ -7,124 +7,81 @@ import (
 	"sync"
 )
 
-const (
-	ChunkSize   = 10 * 1024 * 1024 // 10MB chunks
-	BufferSize  = 5 * 1024 * 1024  // 5MB buffer
-	WorkerCount = 4                // Number of concurrent workers
-)
-
-var (
-	globalContent = Content{}
-	contentMutex  sync.RWMutex
-)
-
-// TODO: Check whyt number chunks does not match file size
-func SplitFileConcurrent(inputPath string) (*Content, error) {
-	fmt.Println("Splitting file", inputPath)
-	file, err := os.Open(inputPath)
+func Splitter(filePath string, chunkSize int, numWorkers int, processor ChunkProcessor) error {
+	// Open file
+	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("failed to open file: %w", err)
 	}
-	defer func() {
-		if cerr := file.Close(); cerr != nil && err == nil {
-			err = cerr
-		}
-	}()
+	defer file.Close()
 
-	// Worker pool pattern
-	chunkChan := make(chan Fragment, WorkerCount)
-	errChan := make(chan error, 1)
-	doneChan := make(chan struct{})
+	// Get file size
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to get file stats: %w", err)
+	}
+	fileSize := fileInfo.Size()
+
+	type chunkWork struct {
+		offset     int64
+		size       int
+		chunkIndex int
+	}
+	workChan := make(chan chunkWork, numWorkers)
 	var wg sync.WaitGroup
+	errChan := make(chan error, numWorkers)
 
-	// Start worker goroutines
-	for range WorkerCount {
+	for i := 0; i < numWorkers; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			worker(inputPath, chunkChan)
+
+			buffer := make([]byte, chunkSize)
+
+			for work := range workChan {
+				sectionReader := io.NewSectionReader(file, work.offset, int64(work.size))
+				
+				n, err := sectionReader.Read(buffer[:work.size])
+				if err != nil && err != io.EOF {
+					errChan <- fmt.Errorf("error reading chunk %d: %w", work.chunkIndex, err)
+					return
+				}
+				
+				if n > 0 {
+					if err := processor(buffer[:n], work.chunkIndex); err != nil {
+						errChan <- fmt.Errorf("error processing chunk %d: %w", work.chunkIndex, err)
+						return
+					}
+				}
+			}
 		}()
 	}
 
-	// Start reader goroutine
-	go func() {
-		defer close(chunkChan)
-		if err := reader(file, chunkChan); err != nil {
-			select {
-			case errChan <- err:
-			default:
-			}
+	// Queue work items
+	chunkIndex := 0
+	for offset := int64(0); offset < fileSize; offset += int64(chunkSize) {
+		size := chunkSize
+		if offset+int64(chunkSize) > fileSize {
+			size = int(fileSize - offset)
 		}
-	}()
+		
+		workChan <- chunkWork{
+			offset:     offset,
+			size:       size,
+			chunkIndex: chunkIndex,
+		}
+		chunkIndex++
+	}
+	close(workChan)
 
-	// Monitor completion
-	go func() {
-		wg.Wait()
-		close(doneChan)
-	}()
+	wg.Wait()
+	close(errChan)
 
-	// Wait for completion or error
 	select {
 	case err := <-errChan:
-		return nil, err
-	case <-doneChan:
-		return &globalContent, nil
+		return err
+	default:
+		return nil
 	}
 }
 
-func reader(file *os.File, chunkChan chan<- Fragment) error {
-	buf := make([]byte, BufferSize)
-	chunkNum := 0
-	currentChunk := make([]byte, 0, ChunkSize)
-
-	for {
-		n, err := file.Read(buf)
-		if err != nil && err != io.EOF {
-			return err
-		}
-		if n == 0 {
-			break
-		}
-
-		remaining := buf[:n]
-		for len(remaining) > 0 {
-			available := min(ChunkSize-len(currentChunk), len(remaining))
-
-			currentChunk = append(currentChunk, remaining[:available]...)
-			remaining = remaining[available:]
-
-			if len(currentChunk) == ChunkSize || (err == io.EOF && len(currentChunk) > 0) {
-				// Make a copy of the chunk data to avoid data races
-				chunkData := make([]byte, len(currentChunk))
-				copy(chunkData, currentChunk)
-
-				chunkChan <- Fragment{
-					Number: chunkNum,
-					Data:   chunkData,
-				}
-				chunkNum++
-				currentChunk = currentChunk[:0] // Reset slice
-			}
-		}
-	}
-	return nil
-}
-
-func worker(basePath string, chunkChan <-chan Fragment) {
-	for fragment := range chunkChan {
-		chunkPath := fmt.Sprintf("%s.part%d", basePath, fragment.Number)
-		fmt.Println("Adding fragment", chunkPath)
-		contentMutex.Lock()
-		globalContent.Fragments = append(globalContent.Fragments, fragment)
-		contentMutex.Unlock()
-		/*
-			if err := os.WriteFile(chunkPath, chunk.Data, 0644); err != nil {
-				select {
-				case errChan <- fmt.Errorf("failed to write chunk %d: %w", chunk.Number, err):
-				default:
-				}
-				return
-			}
-		*/
-	}
-}
