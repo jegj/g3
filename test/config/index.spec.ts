da219987daa309026da4803d77fc9f02ca440b51
Javier Galarza <jegj57@gmail.com>
2025-09-03 07:16:22 -0500

    feat: add zod for schema validation

--- test/config/index.spec.ts
+++ test/config/index.spec.ts
@@ -1,31 +1,43 @@
-import { describe, test } from "node:test";
+import { describe, it } from "node:test";
 import assert from "node:assert";
-import { validateG3Config } from "../../src/config";
+import { createG3Config } from "../../src/config";
+import { z } from "zod";
 
 describe("Config", () => {
-  test("test", () => {
-    assert.strictEqual(1, 1);
-  });
-
-  describe("validateG3Config", () => {
-    test("should throw if GitHubToken is missing", () => {
-      assert.throws(() => validateG3Config({}), {
-        name: "Error",
-        message: "GithubToken is required",
-      });
-    });
-
-    test("should throw if GitHubToken is missing", () => {
+  describe("createG3Config", () => {
+    it("should throw an error with correct message when GitHubToken is missing", () => {
+      const emptyConfig = {};
       assert.throws(
-        () =>
-          validateG3Config({
-            GitHubToken: "github_22faketoken_asieowiafhjls2basdjnnmazxwa",
-          }),
-        {
-          name: "Error",
-          message: "AesKey is required",
+        () => createG3Config(emptyConfig),
+        (error: Error) => {
+          assert.ok(error instanceof z.ZodError);
+          return true;
         },
       );
+    });
+
+    it("should throw an error with correct message when AesKey is missing", () => {
+      const partialConfig = {
+        GitHubToken: "github_22faketoken_asieowiafhjls2basdjnnmazxwa",
+      };
+
+      assert.throws(
+        () => createG3Config(partialConfig),
+        (error: Error) => {
+          assert.ok(error instanceof z.ZodError);
+          return true;
+        },
+      );
+    });
+
+    it("should return a G3Config type if the config properties are valid", () => {
+      const partialConfig = {
+        GitHubToken: "github_22faketoken_asieowiafhjls2basdjnnmazxwa",
+        AesKey: "12345678901234567890123456789012",
+      };
+      const config = createG3Config(partialConfig);
+      assert.strictEqual(config.GitHubToken, partialConfig.GitHubToken);
+      assert.strictEqual(config.AesKey, partialConfig.AesKey);
     });
   });
 });
