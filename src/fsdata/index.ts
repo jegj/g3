b3ff05733424873172d1b10fc3718912ee261443
Javier Galarza <jegj57@gmail.com>
2025-09-24 06:55:15 -0500

    feat: add helper function to the fsdata

--- src/fsdata/index.ts
+++ src/fsdata/index.ts
@@ -1,7 +1,18 @@
 import fs from "fs";
+import { stat } from "fs/promises";
 import { G3File } from "../g3file";
 
 export const getFileSize = async (g3File: G3File): Promise<number> => {
   const stats = await fs.promises.stat(g3File.filepath);
   return stats.size;
+};
+
+// For small files
+export const getFileContent = async (g3File: G3File): Promise<Buffer> => {
+  return fs.promises.readFile(g3File.filepath);
+};
+
+export const hasEntry = async (g3FIle: G3File): Promise<boolean> => {
+  stat(g3FIle.g3Filepath);
+  return true;
 };
