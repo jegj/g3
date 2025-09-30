5a9441d98e254a7ec0f9e1617b8a32a1d13ec7fc
Javier Galarza <jegj57@gmail.com>
2025-09-30 07:40:54 -0500

    feat: add ls command

--- src/cmd/ls.ts
+++ src/cmd/ls.ts
@@ -1,7 +1,14 @@
 import { ArgumentsCamelCase } from "yargs";
 import { createConfigFromArgv } from "../config";
+import { createG3EntriesFactory } from "../fsdata";
+import { G3Dependecies } from "../types";
 
 export default async function ls(argv: ArgumentsCamelCase) {
   const config = createConfigFromArgv(argv);
-  console.log("ls command: listing files in storage", config);
+  const dependencies: G3Dependecies = { config };
+  const getG3Entries = createG3EntriesFactory(dependencies);
+  const entries = await getG3Entries();
+  for (const entry of entries) {
+    console.log(entry);
+  }
 }
