da7b726c20c92ec8e4961662335b1a78846445b6
Javier Galarza <jegj57@gmail.com>
2025-10-09 06:58:56 -0500

    refactor: use different function

--- src/cmd/rm.ts
+++ src/cmd/rm.ts
@@ -3,22 +3,28 @@ import { createConfigFromArgv } from "../config";
 import { deleteG3Entry } from "../fsdata";
 import { deleteGistFactory } from "../gist";
 import { G3Dependecies } from "../types";
-import { createG3FileFactory } from "../g3file";
+import { createG3FileFactory, G3File } from "../g3file";
 import { GistDataEntry } from "../fsdata/types";
 
 export default async function rm(argv: ArgumentsCamelCase) {
   const config = createConfigFromArgv(argv);
   const dependencies: G3Dependecies = { config };
-  const deleteGist = deleteGistFactory(dependencies);
   const createG3File = createG3FileFactory(dependencies);
+  const deleteG3FileGists = deleteG3FileGistsFactory(dependencies);
   const file = argv.file as string;
   const g3File = await createG3File(file);
-
-  //TODO: What if fail because one call fail
-  await Promise.all(
-    g3File.filesystemDataEntry.entries.map((e: GistDataEntry) =>
-      deleteGist(e.id),
-    ),
-  );
+  await deleteG3FileGists(g3File);
   await deleteG3Entry(g3File);
+}
+
+function deleteG3FileGistsFactory(dependencies: G3Dependecies) {
+  const deleteGist = deleteGistFactory(dependencies);
+  return async function deleteG3FileGists(g3File: G3File): Promise<void> {
+    //TODO: What if fail because one call fail
+    await Promise.all(
+      g3File.filesystemDataEntry.entries.map((e: GistDataEntry) =>
+        deleteGist(e.id),
+      ),
+    );
+  };
 }
