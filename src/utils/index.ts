7bfda3733c36a1eb5af58637e95ce80e731aa042
Javier Galarza <jegj57@gmail.com>
2025-11-12 07:48:04 -0500

    feat: add new utils function for tmp folders

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,22 @@
+import { mkdir } from "fs/promises";
 import os from "os";
-import path from "path";
+import path, { join } from "path";
 
 export function resolvePath(filePath: string): string {
   if (filePath.startsWith("~")) {
     return path.join(os.homedir(), filePath.slice(1));
   }
   return path.resolve(filePath);
+}
+
+export async function createTempFolder(
+  location: string,
+  folderName: string,
+): Promise<string> {
+  const hiddenFolderName = folderName.startsWith(".")
+    ? folderName
+    : `.${folderName}`;
+  const folderPath = join(location, hiddenFolderName);
+  await mkdir(folderPath, { recursive: true });
+  return folderPath;
 }
