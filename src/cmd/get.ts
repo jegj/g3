a700dfa8ea2afd66db8be1c4c153f177a7146427
Javier Galarza <jegj57@gmail.com>
2025-11-14 08:04:19 -0500

    feat: define structure for the get

--- src/cmd/get.ts
+++ src/cmd/get.ts
@@ -1,13 +1,10 @@
+import { spawn } from "child_process";
 import { ArgumentsCamelCase } from "yargs";
 import { createConfigFromArgv } from "../config";
-import { encryptAESGCM } from "../crypto";
 import { g3Error } from "../error";
-import { appendG3FSEntry, getFileContent, getFileSizeMb } from "../fsdata";
-import { GistDataEntry } from "../fsdata/types";
 import { createG3FileFactory } from "../g3file";
-import { createGistFactory } from "../gist";
-import { GistResponse } from "../gist/types";
 import { G3Dependecies } from "../types";
+import { createTempFolder, resolvePath } from "../utils";
 
 // The idea to use the gist_pull_url and do the merging locally
 export default async function get(argv: ArgumentsCamelCase) {
@@ -15,15 +12,52 @@ export default async function get(argv: ArgumentsCamelCase) {
   const dependencies: G3Dependecies = { config };
   const createG3File = createG3FileFactory(dependencies);
   const file = argv.file as string;
+  const fileDestination = resolvePath(argv.destination as string);
   const g3File = await createG3File(file);
-
+  const temporalFolder = await createTempFolder(
+    fileDestination,
+    g3File.filename,
+  );
+  console.log(`Getting file ${file} to ${temporalFolder}/`);
   if (g3File.exists) {
-    if (g3File.hasMultipleFiles()) {
+    if (g3File.hasMultipleGistEntries()) {
     } else {
-      //TODO: CHECK IF USE WORKER OR PROCESS
-      //TODO: GIT CLONE <PULL_URL> <FOLDER> This clone all the entries inside the gist, sort later
+      await gitClone(
+        g3File.filesystemDataEntry.entries[0].gistPullUrl,
+        temporalFolder,
+      );
+      // TODO: Implement merging logic
     }
   } else {
     throw g3Error(`File ${file} does not exist in g3data.`);
   }
+}
+
+async function gitClone(url: string, folder: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const gitProcess = spawn("git", ["clone", url, folder]);
+
+    let stderr = "";
+
+    gitProcess.stdout.on("data", (data) => {
+      console.log(data.toString());
+    });
+
+    gitProcess.stderr.on("data", (data) => {
+      stderr += data.toString();
+      console.error(data.toString());
+    });
+
+    gitProcess.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`git clone failed with code ${code}: ${stderr}`));
+      }
+    });
+
+    gitProcess.on("error", (error) => {
+      reject(new Error(`Failed to start git process: ${error.message}`));
+    });
+  });
 }
