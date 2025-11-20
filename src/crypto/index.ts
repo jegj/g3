f73885758a18f396c2650c5ef5114e944d483bc3
Javier Galarza <jegj57@gmail.com>
2025-11-20 08:03:39 -0500

    refactor: use new functions

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -2,6 +2,7 @@ import * as crypto from "crypto";
 import { g3Error } from "../error";
 
 const algorithm = "aes-256-cbc";
+/*
 const iterations = 100000;
 const keyLength = 32;
 const ivLength = 16;
@@ -50,4 +51,25 @@ export function decryptAESGCM(ciphertext: Buffer, password: Buffer): Buffer {
       errorMessage,
     );
   }
+}
+
+*/
+
+export function encryptAESGCM(text: string, password: string): string {
+  const key = Buffer.from(password, "hex");
+  const iv = crypto.randomBytes(16); // Generate a random IV for each encryption
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return iv.toString("hex") + encrypted; // Prepend IV for decryption
+}
+
+export function decryptAESGCM(encryptedText: string, password: string): string {
+  const key = Buffer.from(password, "hex");
+  const ivFromCipher = Buffer.from(encryptedText.slice(0, 32), "hex");
+  const encryptedData = encryptedText.slice(32);
+  const decipher = crypto.createDecipheriv(algorithm, key, ivFromCipher);
+  let decrypted = decipher.update(encryptedData, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
 }
