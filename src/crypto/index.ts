7f6a5e22e49693afd0eaa08fb547cfce8365ebc6
Javier Galarza <jegj57@gmail.com>
2025-11-21 06:59:27 -0500

    refactor: crypto module

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -1,64 +1,11 @@
 import * as crypto from "crypto";
-import { g3Error } from "../error";
 
-const algorithm = "aes-256-cbc";
-/*
-const iterations = 100000;
-const keyLength = 32;
-const ivLength = 16;
-
-function deriveKeyAndIV(password: Buffer, salt: Buffer) {
-  const key = crypto.pbkdf2Sync(
-    password,
-    salt,
-    iterations,
-    keyLength,
-    "sha256",
-  );
-  const iv = key.slice(0, ivLength); // Use the first 16 bytes as the IV
-  return { key, iv };
-}
-
-export function encryptAESGCM(plaintext: Buffer, password: Buffer): Buffer {
-  const salt = crypto.randomBytes(16);
-  const { key, iv } = deriveKeyAndIV(password, salt);
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
-  const encryptedData = Buffer.concat([
-    cipher.update(plaintext),
-    cipher.final(),
-  ]);
-
-  return encryptedData;
-}
-
-export function decryptAESGCM(ciphertext: Buffer, password: Buffer): Buffer {
-  const salt = ciphertext.subarray(0, 16);
-  const encryptedData = ciphertext.subarray(16);
-  const { key, iv } = deriveKeyAndIV(password, salt);
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-
-  try {
-    const decryptedData = Buffer.concat([
-      decipher.update(encryptedData),
-      decipher.final(),
-    ]);
-    return decryptedData;
-  } catch (error) {
-    const errorMessage =
-      error instanceof Error ? error.message : "Unknown error";
-    throw g3Error(
-      "Decryption failed. Invalid password or corrupted data.",
-      errorMessage,
-    );
-  }
-}
-
-*/
+const ALG = "aes-256-cbc";
 
 export function encryptAESGCM(text: string, password: string): string {
   const key = Buffer.from(password, "hex");
   const iv = crypto.randomBytes(16); // Generate a random IV for each encryption
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const cipher = crypto.createCipheriv(ALG, key, iv);
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
   return iv.toString("hex") + encrypted; // Prepend IV for decryption
@@ -68,7 +15,7 @@ export function decryptAESGCM(encryptedText: string, password: string): string {
   const key = Buffer.from(password, "hex");
   const ivFromCipher = Buffer.from(encryptedText.slice(0, 32), "hex");
   const encryptedData = encryptedText.slice(32);
-  const decipher = crypto.createDecipheriv(algorithm, key, ivFromCipher);
+  const decipher = crypto.createDecipheriv(ALG, key, ivFromCipher);
   let decrypted = decipher.update(encryptedData, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
