a2aa9007baa659f12bb3437e3dcbf8dc70c8bfd5
Javier Galarza <jegj57@gmail.com>
2026-01-13 07:32:58 -0500

    chore: add todo note

--- src/cmd/ls.ts
+++ src/cmd/ls.ts
@@ -8,6 +8,7 @@ export default async function ls(argv: ArgumentsCamelCase) {
   const dependencies: G3Dependecies = { config };
   const getG3Entries = getG3EntriesFactory(dependencies);
   const showGist = argv.gist as boolean;
+  //TODO: Add gist count and description display
   const entries = await getG3Entries();
   for (const entry of entries) {
     console.log(entry.filename);
